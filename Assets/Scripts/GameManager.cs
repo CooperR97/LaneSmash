aedba3341ed65745b5c2e2e6a715ab9fd7729cf9
Cooper Redfern <credfern002@drury.edu>
2019-07-26 14:12:07 -0500

    DefaultLaneSmash
    Nothing changed from tutorial. Game adapted from tutorial by Brackeys found at: https://www.youtube.com/watch?v=j48LtUkZRjU&list=PLPV2KyIb3jR5QFsefuO2RlAgWEz6EvVi6&index=1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	bool gameHasEnded = false;
+
+	public float restartDelay = 1f;
+
+	public GameObject completeLevelUI;
+
+	public void CompleteLevel(){
+		Debug.Log("Level won!");
+		completeLevelUI.SetActive(true);
+	}
+    
+	public void EndGame(){
+
+		if(gameHasEnded == false){
+			gameHasEnded = true;
+			Debug.Log("Game Over.");
+			Invoke("Restart", restartDelay);
+		}
+
+	}
+
+	void Restart(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+}
